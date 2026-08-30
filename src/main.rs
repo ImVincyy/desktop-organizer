c65be744a9f91eec7530a7a18b19e7b5c3d6870f
ShoboDev <zezinho1703@gmail.com>
2026-08-30 11:10:57 -0300

    File type identification workinnnn

--- src/main.rs
+++ src/main.rs
@@ -5,16 +5,65 @@ use colored::Colorize;
 
 use crate::simple_user_input::get_input;
 
-fn main() -> std::io::Result<()> {
-    println!("{}", "WARNING : This application works by deleting and copying files into a new place.".yellow());
+#[derive(Debug)]
+enum FolderTypes {
+    Applications, // atalhos tem o tipo de extensão de arquivo .lnk  |  a maioria são de executáveis, então, acho que é seguro apostar nisso
+    Code,
+    Media,
+    Images,
+    Misc
+}
 
-    let binding = get_input("Please specify the directory where you wish to organize the files.");
+// fazer um algoritmo capaz de distuinguir o determinado uso de uma pasta analisando os conteúdos dentro dela.
+
+fn main() -> std::io::Result<()> {
+    print!("\n");
+    let current_user = get_current_user()?;
+
+    let mut binding: String = get_input("Please specify the directory where you wish to organize the files (Default is desktop)");
+    if binding.len() <= 1 {
+        binding = format!("C:\\Users\\{}\\Desktop", current_user);
+    }
     let to_organize_dir = Path::new(&binding);
 
     let my_files = Path::new(&to_organize_dir);
 
-    if let Ok(_) = create_dir(my_files) {
-        println!("Sucess!")
+    //fs::rename("a.txt", "moved_new_dir/b.txt")?;
+    read_data_log(my_files);
+    move_dir_files(my_files)?;
+
+    println!("{}", get_current_user().unwrap());
+
+    Ok(())
+}
+
+fn read_data_log(f: &Path) {
+    if let Some(n) = f.file_name() {
+        println!("Reading {} data...", n.to_str().unwrap());
+    }
+}
+
+fn get_current_user() -> Result<String, Error> {
+    match std::env::var("USERNAME") {
+        Ok(username) => Ok(username),
+        Err(e) => Err(Error::other(format!("Erro: {}", e)))
+    }
+}
+
+fn move_dir_files(path: &Path) -> std::io::Result<()> {
+    if let Ok(n) = fs::read_dir(path) {
+        for input in n {
+            let input = input?;
+            let path = input.path();
+
+            if path.is_file() {
+                println!("Found : [{}]", path.display());
+                //println!("Extension : .{:?}", path.extension().unwrap().to_str().unwrap())
+            } else if path.is_dir() {
+                println!("Found : [{}]", path.display());
+                move_dir_files(&path)?;
+            }
+        }
     }
 
     Ok(())
